2865999ac25b49cbed1e28cecc07761347a3f39f
yuma-shudo <shudo220t@gmail.com>
2026-02-22 15:33:07 +0900

    AC: dp_a

---
+++ AtCoder/2026-02-22/dp_a/main.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    n = int(input())
+    h = list(map(int, input().split()))
+    dp = [0]*(n)
+    dp[1] = abs(h[1] - h[0])
+    for i in range(2, n):
+        dp[i] = min(dp[i-2] + abs(h[i] - h[i-2]), dp[i-1] + abs(h[i] - h[i-1]))
+    print(dp[n-1])
+
+main()
